c0e4140aa1f46d3e06c27685b90f48cbe3ee2ed5
Nam <namdeveloper.ca@gmail.com>
2022-12-22 10:52:47 +0700

    Active Link

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -2,44 +2,46 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Container>
-        <Link className='navbar-brand' to='/'>
-          Hỏi Dân IT
-        </Link>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse>
-          <Nav className='me-auto'>
-            <Link className='nav-link' to='/'>
-              Home
-            </Link>
-            <Link className='nav-link' to='/profile'>
-              Users
-            </Link>
-            <Link className='nav-link' to='/admin'>
-              Admin
-            </Link>
-          </Nav>
-          <Nav>
-            <NavDropdown title='Settings'>
-              <Link to='/' className='dropdown-item'>
-                Log in
-              </Link>
-              <Link to='/' className='dropdown-item'>
-                Log out
-              </Link>
-              <Link to='/' className='dropdown-item'>
-                Profile
-              </Link>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='header'>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Link className='navbar-brand' to='/'>
+            Hỏi Dân IT
+          </Link>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse>
+            <Nav className='me-auto'>
+              <NavLink className='nav-link' to='/'>
+                Home
+              </NavLink>
+              <NavLink className='nav-link' to='/profile'>
+                Users
+              </NavLink>
+              <NavLink className='nav-link' to='/admin'>
+                Admin
+              </NavLink>
+            </Nav>
+            <Nav>
+              <NavDropdown title='Settings'>
+                <Link to='/hehe' className='dropdown-item'>
+                  Log in
+                </Link>
+                <Link to='/hihi' className='dropdown-item'>
+                  Log out
+                </Link>
+                <Link to='/haha' className='dropdown-item'>
+                  Profile
+                </Link>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
