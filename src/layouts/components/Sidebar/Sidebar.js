24ba68fcf5024c88a149cc154ac36b06e40e9f34
Nam <namdeveloper.ca@gmail.com>
2022-12-24 19:42:15 +0700

    Sidebar without Link Active

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -2,10 +2,15 @@ import styles from './Sidebar.module.scss'
 import classnames from 'classnames/bind'
 import { Logo } from '~/assets/svg'
 import { Link } from 'react-router-dom'
+import { SidebarData } from './SidebarData'
+import { useState } from 'react'
 
 const cx = classnames.bind(styles)
 
 function Sidebar() {
+  const [subMenu, setSubMenu] = useState(false)
+
+  const showSubMenu = () => setSubMenu(!subMenu)
   return (
     <aside className={cx('sidebar')}>
       <div className={cx('sidebar-logo')}>
@@ -14,44 +19,28 @@ function Sidebar() {
         </Link>
       </div>
       <ul className={cx('menu-inner')}>
-        <li className={cx('menu-item', 'active')}>
-          <a className={cx('menu-link')}>
-            <div>Dashboard</div>
-          </a>
-        </li>
-        <li className={cx('menu-item')}>
-          <a className={cx('menu-link')}>
-            <div>Layouts</div>
-          </a>
-
-          <ul className={cx('menu-sub')}>
-            <li className={cx('menu-item')}>
-              <a className={cx('menu-link')}>
-                <div>Without menu</div>
-              </a>
-            </li>
-            <li className={cx('menu-item')}>
-              <a className={cx('menu-link')}>
-                <div>Without navbar</div>
-              </a>
-            </li>
-            <li className={cx('menu-item')}>
-              <a className={cx('menu-link')}>
-                <div>Container</div>
-              </a>
-            </li>
-            <li className={cx('menu-item')}>
-              <a className={cx('menu-link')}>
-                <div>Fluid</div>
-              </a>
-            </li>
-            <li className={cx('menu-item')}>
-              <a className={cx('menu-link')}>
-                <div>Blank</div>
-              </a>
-            </li>
-          </ul>
-        </li>
+        {SidebarData.map((item, i) => (
+          <li key={i} className={cx('menu-item', i === 2 && 'active')}>
+            <a className={cx('menu-link')}>
+              {item.icon}
+              <div className={cx('menu-link-title')}>{item.title}</div>
+              <div className={cx('menu-dropdown-icon')} onClick={item.subMenu && showSubMenu}>
+                {item.subMenu && subMenu ? item.iconOpened : item.subMenu ? item.iconClosed : null}
+              </div>
+            </a>
+            {item.subMenu && subMenu && (
+              <ul className={cx('menu-sub')}>
+                {item.subMenu.map((child, i) => (
+                  <li key={i} className={cx('menu-item')}>
+                    <a className={cx('menu-link')}>
+                      <div>{child.title}</div>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
     </aside>
   )
--- src/layouts/components/Sidebar/SidebarData.js
+++ src/layouts/components/Sidebar/SidebarData.js
@@ -2,25 +2,25 @@ import React from 'react'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import * as IoIcons from 'react-icons/io'
+import * as RiIcons from 'react-icons/ri'
 
 export const SidebarData = [
   {
     title: 'Home',
     path: '/',
     icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text',
   },
   {
     title: 'Reports',
     path: '/reports',
     icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text',
   },
   {
     title: 'Products',
     path: '/products',
     icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text',
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
     subMenu: [
       {
         code: 'en',
@@ -42,28 +42,21 @@ export const SidebarData = [
         code: 'po',
         title: 'Svenska (Sverige)',
       },
-      {
-        code: 'in ',
-        title: 'Русский (Россия)',
-      },
     ],
   },
   {
     title: 'Team',
     path: '/team',
     icon: <IoIcons.IoMdPeople />,
-    cName: 'nav-text',
   },
   {
     title: 'Messages',
     path: '/messages',
     icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text',
   },
   {
     title: 'Support',
     path: '/support',
     icon: <IoIcons.IoMdHelpCircle />,
-    cName: 'nav-text',
   },
 ]
