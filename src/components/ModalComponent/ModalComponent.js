c0f78df944e27811ec6e444d3b98cf1b0c9076d0
Nam <namdeveloper.ca@gmail.com>
2022-12-25 17:30:30 +0700

    Modal Style

--- src/components/ModalComponent/ModalComponent.js
+++ src/components/ModalComponent/ModalComponent.js
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import styles from './ModalComponent.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 function ModalComponent() {
   const [show, setShow] = useState(false)
@@ -14,11 +19,42 @@ function ModalComponent() {
         Launch demo modal
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} size='lg'>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form className={cx('form')}>
+            <Form.Group className={cx('form-group-full')} controlId='userEmail'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type='email' placeholder='Enter email' />
+            </Form.Group>
+
+            <Form.Group className={cx('form-group-half')} controlId='userPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Password' />
+            </Form.Group>
+
+            <Form.Group className={cx('form-group-half')} controlId='userName'>
+              <Form.Label>UserName</Form.Label>
+              <Form.Control type='text' placeholder='Username' />
+            </Form.Group>
+
+            <Form.Group className={cx('form-group-half')} controlId='userImage'>
+              <Form.Label>Pick User's Avatar</Form.Label>
+              <Form.Control type='file' />
+            </Form.Group>
+
+            <Form.Group className={cx('form-group-half')} controlId='userRole'>
+              <Form.Label>Role</Form.Label>
+              <Form.Select>
+                <option>Open this role menu</option>
+                <option value='user'>USER</option>
+                <option value='admin'>ADMIN</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
