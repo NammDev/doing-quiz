d9bcb0adbc1cc35fdb3cdba181a1b12c6e8e95e1
Nam <namdeveloper.ca@gmail.com>
2023-01-01 15:01:33 +0700

    Show Answer after submit

--- src/pages/Quiz/ModalAnswer.js
+++ src/pages/Quiz/ModalAnswer.js
@@ -1,16 +1,23 @@
 import { ModalComponent } from '~/components/ModalComponent'
 
 function ModalAnswer({ show, setShow, data }) {
+  const handleSubmit = () => {
+    const result = data.quizData.map((quiz) => ({
+      quiz: quiz.questionId,
+      correctAnswer: quiz.systemAnswers[0].description,
+    }))
+    console.log(result.map((res) => console.log(res)))
+  }
   return (
     <ModalComponent
       heading={'Your Result'}
       footer={'Show Answer'}
       show={show}
       onClose={() => setShow(false)}
-      onSubmit={() => console.log(data.quizData)}
+      onSubmit={handleSubmit}
     >
       <h4>
-        {data.countCorrect}/ {data.countTotal}
+        {data.countCorrect} / {data.countTotal}
       </h4>
       <p>Total Correct Answer: {data.countCorrect}</p>
       <p>Total Question: {data.countTotal}</p>
